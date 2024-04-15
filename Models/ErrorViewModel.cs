31c453822449e48b74eb4f60356e03727e77c029
Guilherme do Amaral Gonçalves da Silva <guilhemesilva2227@gmail.com>
2024-04-15 19:24:37 -0300

    Second Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AMST4Carousel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
